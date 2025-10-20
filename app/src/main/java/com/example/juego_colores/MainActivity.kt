295afe8aec9db144bad6b8f15e9270c6cf4e7485
Angelica <acastilloto@ulasalle.edu.pe>
2025-10-19 19:39:45 -0500

    Agregando AlertDialog

--- app/src/main/java/com/example/juego_colores/MainActivity.kt
+++ app/src/main/java/com/example/juego_colores/MainActivity.kt
@@ -1,9 +1,29 @@
 package com.example.juego_colores
-
+import android.widget.Button
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val options = resources.getStringArray(R.array.array_dialog_add_options)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_button, null)
+
+        val dialog = MaterialAlertDialogBuilder(this)
+            .setTitle(R.string.dialog_add_present_title)
+            .setItems(options) { _, index ->
+
+            }
+            .setView(dialogView)
+            .create()
+
+        dialog.show()
+
+        val btnIniciar = dialogView.findViewById<Button>(R.id.btn_Iniciar)
+        btnIniciar.setOnClickListener {
+            dialog.dismiss()
         }
     }
+}
